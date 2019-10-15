9965f44e8124a51ff9223d9350be10c0c3c8686e
Raúl <raul@ideas24h.com>
2019-10-16 01:21:36 +0200

    primeros cambios,
    cambio logo, nombre de web, url y poco mas

--- gridsome.config.js
+++ gridsome.config.js
@@ -14,9 +14,9 @@ const postcssPlugins = [
 if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
 
 module.exports = {
-  siteName: 'Gridsome Portfolio Starter',
-  siteDescription: 'A simple portfolio theme for Gridsome powered by Tailwind CSS v1',
-  siteUrl: 'https://gridsome-portfolio-starter.netlify.com',
+  siteName: 'Mobile Wifi Spain',
+  siteDescription: 'RENT MOBILE WIFI SPAIN AND AVOID ROAMING FEES WHEN TRAVELLING IN SPAIN.',
+  siteUrl: 'https://mobile-wifi-spain.netlify.com',
   plugins: [
     {
       use: '@gridsome/vue-remark',

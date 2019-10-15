ae1a0ba7a7427b77ea6165470ee00d28404bc20e
Raúl <raul@ideas24h.com>
2019-10-16 01:35:16 +0200

    quito el video

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -66,9 +66,10 @@
           <div class="flex-1 text-lg sm:text-xl ml-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quae architecto minus molestias iusto ducimus fuga quo quia libero dignissimos, fugiat modi assumenda debitis laboriosam.</div>
         </div>
 
+        <!--  quito el video
         <div class="responsive-container">
           <iframe class="responsive-iframe w-full h-full top-0 left-0 border-0" src="https://www.youtube.com/embed/uHo6o1TNQeE" style="border:0;" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
+        </div> -->
       </div> <!-- end get-to-know me -->
     </div>
 
